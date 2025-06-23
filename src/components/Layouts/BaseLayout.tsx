dc115ce4019d692611d31d45bb597bc8ea2e51d4
CodeBunk <asreddy5270@gmail.com>
2025-06-23 10:44:58 +0530

    Added base layout changes

--- src/components/Layouts/BaseLayout.tsx
+++ src/components/Layouts/BaseLayout.tsx
@@ -33,8 +33,12 @@ const BaseLayout = () => {
     <div className='flex flex-col justify-between md:h-screen'>
       {/* <Navbar/> */}
       <div onMouseEnter={() => setValue(true)} onTouchStart={() => setValue(true)} onClick={() => setValue(true)} className={`   absolute bottom-4 right-4  z-40 flex items-center justify-center  hover:w-20 hover:h-20 w-10 h-10   transition-all duration-300 cursor-pointer  ${Value ? " max-md:w-20 max-md:h-20  " : "  "}  `}>
-        <div onClick={() => { Value ? setValue(true) : console.log("not setting value") }} className={`" z-[50] border border-black/20 font-poppins relative rounded-full  hover:scale-[105%] transition-all duration-300  w-10 h-10  flex items-center justify-center cursor-pointer  ${Value ? " scale-[70%] bg-red text-white " : "bg-white text-black"} "`}>
+        <div onClick={() => { Value ? setValue(true) : console.log("not setting value") }} className={`" relative  z-[50] border border-black/20 font-poppins  rounded-full  hover:scale-[105%] transition-all duration-300  w-10 h-10  flex items-center justify-center cursor-pointer  ${Value ? " scale-[70%] bg-red text-white " : "bg-white text-black"} "`}>
+          {!Value ? <div className=" -z-10  absolute  inline-flex h-full w-full animate-ping rounded-full bg-white opacity-asdas0 "></div>
+            : null}
           {!Value ? "🕹️" : " X "}
+
+
 
         </div>
         <div aria-disabled={!Value} onClick={() => HandleLocation("/food")} className={` border border-black/20 absolute ${Value ? " bottom-20 right-2 opacity-100 " : " bottom-5 right-5 opacity-0  "} text-2xl    rounded-full bg-white hover:scale-[120%] transition-all duration-300 text-black w-10 h-10  flex items-center justify-center cursor-pointer `}>
